d75f830f1cadb1754735481b8f7b5f54ee1d92cc
ssimmons9212 <sammsim87@gmail.com>
2020-05-06 20:23:37 -0500

    added KFold CV

--- Prediction_Codes/preprocess.py
+++ Prediction_Codes/preprocess.py
@@ -5,7 +5,7 @@ def prep_data(df):
     df = df.assign(l3l1width = df["Length3"]/df["Length1"]* df["Width"])
     df = df.assign(l3l1height = df["Length3"]/df["Length1"]* df["Height"])
 
-    X = df[["Width","Height","l3l1width","l3l1height"]].values
+    X = df[["Width","Height","hw","l3l1width","l3l1height"]].values
     Y = df["Weight"].values
     y=Y.astype(int)
 
--- Prediction_Codes/train.py
+++ Prediction_Codes/train.py
@@ -5,13 +5,27 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from joblib import dump
 from preprocess import prep_data
+from sklearn.model_selection import KFold
+from sklearn.metrics import classification_report
 
 df = pd.read_csv(("fish_participant.csv"))
 
 X, y = prep_data(df)
 
+kf = KFold(n_splits = 20, shuffle = True, random_state = 55)
+kf.get_n_splits(X)
+
+print(kf)
+
+for train_index, test_index in kf.split(X):
+    print("TRAIN:", train_index, "TEST:", test_index)
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], [test_index]
+    
+
 wrf = RandomForestClassifier(class_weight = "balanced")
-wrf.fit(X, y)
+wrf.fit(X_train, y_train)
+y_pred = wrf.predict(X_test)
 
 # nb = GaussianNB()
 # nb.fit(X, y)
